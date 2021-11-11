75199ac435bdc91dfff939ee5704585df8691d3c
schmidtjohannes <johannes.schmidt.delaunay@gmail.com>
2021-11-11 10:32:42 -0300

    trend follow with kraken

--- kraken/trendfollow-broker.py
+++ kraken/trendfollow-broker.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     api.load_key('.kraken.api.key')
     k = KrakenAPI(api)
     bot_pair = 'ETHUSD'
+    #bot_pair = 'MINAUSD'
 
 engine = sqlalchemy.create_engine('sqlite:///'+bot_pair+'stream.db')
 
@@ -44,41 +45,58 @@ def ws_thread(*args):
     ws.run_forever()
 
 # Start a new thread for the WebSocket interface
-_thread.start_new_thread(ws_thread, ())
+#_thread.start_new_thread(ws_thread, ())
 # todo: https://support.kraken.com/hc/en-us/articles/360044504011-WebSocket-API-unexpected-disconnections-from-market-data-feeds
 
-def create_order(qunatity, type='buy'):
-#    order = k.add_standard_order(pair=bot_pair,
-#        ordertype='market',
-#        type='buy',
-#        valume=qunatity)
-    print('create order: ' + type)
-    d = {'transactTime': pd.to_datetime(time.now(), unit='ms') }
-    df = pd.DataFrame(data=d, index=[0])
-    return df
+#time.sleep(15)
+
+def create_order(quantity, type='buy'):
+    print('will try to create order via API')
+    order = k.add_standard_order(bot_pair,
+        type,
+        'market',
+        quantity,
+        validate=False)
+    if 'txid' not in order:
+        raise Exception('no transaction ID found after order creation')
+    print('order type:' + type)
+    order['transactTime'] = time.time()
+    return order
 
 def strategy(entry, lookback, qty, open_position=False):
     while True:
         # get live data
         df = pd.read_sql(bot_pair, engine)
+        print('current: ' + str(df.iloc[-1:].Price.values))
         lookbackperiod = df.iloc[-lookback:]
         cumret = (lookbackperiod.Price.pct_change() +1).cumprod() -1
+        print('cumret: ' + str(cumret[cumret.last_valid_index()]))
         if not open_position:
             if cumret[cumret.last_valid_index()] > entry:
                 order = create_order(qty, type='buy')
                 print(order)
                 open_position = True
-                break
+                # now we have our open position so let's check when to sell
+                continue
         if open_position:
             while True:
                 df = pd.read_sql(bot_pair, engine)
-                sincebuy = df.loc[df.Time > pd.to_datetime(order['transactTime'], unit='ms')]
+                print('current: ' + str(df.iloc[-1:].Price.values))
+                sincebuy = df.loc[df.Time > pd.to_datetime(order['transactTime'], unit='s')]
                 if len(sincebuy) > 1:
                     sincebuyret = (sincebuy.Price.pct_change() +1).cumprod() -1
                     last_entry = sincebuyret[sincebuyret.last_valid_index()]
-                    if last_entry > 0.0015 or last_entry < 0.0015:
+                    print('last_entry: ' + str(last_entry))
+                    if last_entry == 0:
+                        time.sleep(1)
+                        continue
+                    if last_entry > 0.0015 or last_entry < -0.0015:
                         order = create_order(qty, type='sell')
                         print(order)
+                        open_position = False
                         break
+                time.sleep(1)
+            break
+        time.sleep(1)
 
-strategy(0.001, 60, 0.001)
+strategy(0.001, 60, 0.004)
--- kraken/trendfollow-reader.py
+++ kraken/trendfollow-reader.py
@@ -4,7 +4,10 @@ import pandas as pd
 import sqlalchemy
 import json
 
-engine = sqlalchemy.create_engine('sqlite:///ETHUSDstream.db')
+bot_pair = 'ETHUSD'
+#bot_pair = 'MINAUSD'
+
+engine = sqlalchemy.create_engine('sqlite:///'+bot_pair+'stream.db')
 
 def createframe(msg, symbole):
     d = {'symbole': symbole, 'Price': msg[0], 'Time': msg[2], 'BuySell': msg[3], 'OrderType': msg[4] }
@@ -23,20 +26,22 @@ def createframe(msg, symbole):
 
 # Connect to WebSocket API and subscribe to trade feed for XBT/USD and XRP/USD
 ws = create_connection("wss://ws.kraken.com/")
-ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["ETH/USD"]}')
+splitat = 3
+left, right = bot_pair[:splitat], bot_pair[splitat:]
+ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["'+left+'/'+right+'"]}')
 
 # Infinite loop waiting for WebSocket data
 while True:
     msg = ws.recv()
     response_ = json.loads(str(msg))
-    print(response_)
+    #print(response_)
     if 'event' in response_:
-        print('not interested in events')
+        #print('not interested in events')
         continue
 #    if response_['event'] == 'heartbeat' or response_['event'] == 'systemStatus' or  response_['event'] == 'subscriptionStatus':
 #        continue
     
     frame = createframe(response_[1][0], response_[3])
-    frame.to_sql('ETHUSD', engine, if_exists='append', index=False)
+    frame.to_sql(bot_pair, engine, if_exists='append', index=False)
     print(frame)
 
