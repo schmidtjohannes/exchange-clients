2a5ad5fa34fe74fca74410f017b6050ba784f75a
schmidtjohannes <johannes.schmidt.delaunay@gmail.com>
2021-12-04 12:16:40 -0300

    correct total calc

--- binance/get_all_orders.py
+++ binance/get_all_orders.py
@@ -4,6 +4,13 @@ import pandas as pd
 
 import sys
 
+def calcTotal(row):
+    if row.isBuyer:
+        return (row['price'] * row['qty']) - (row['commission'] * row['price'])
+    else:
+        return (row['price'] * row['qty']) - row['commission']
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read_file(open('.binance'))
@@ -19,18 +26,21 @@ if __name__ == '__main__':
     df.drop(columns=['id','orderListId','quoteQty','commissionAsset','isBestMatch'], inplace=True)
 
     # time range
-    df = df.loc['2021-12-02':'2021-12-02']
-    
+    df = df.loc['2021-12-03 00:00':'2021-12-04']
     print(df)
     # remove first row if SELL
-    #df = df.tail(len(df) - 1)
+    if not df.iloc[0].isBuyer:
+        df = df.tail(len(df) - 1)
+    df = df.tail(len(df) - 1)
     # remove last row if BUY
-    #df = df.head(len(df) - 1)
+    if df.iloc[-1].isBuyer:
+        df = df.head(len(df) - 1)
+    print(df)
     df["price"] = pd.to_numeric(df["price"], downcast="float")
     df["qty"] = pd.to_numeric(df["qty"], downcast="float")
     df["commission"] = pd.to_numeric(df["commission"], downcast="float")
-    df['total'] = (df['price'] * df['qty']) - df['commission']
-    print(df)
+    #df['total'] = (df['price'] * df['qty']) - df['commission']
+    df['total'] = df.apply(calcTotal, axis=1)
 
     buys = df[df["isBuyer"] == True]
     sells = df[df["isBuyer"] == False]
@@ -40,3 +50,8 @@ if __name__ == '__main__':
 
     print("Total in $ " + str(sells['total'].sum() - buys['total'].sum()))
     print("Total in % " + str((sells['total'].sum() - buys['total'].sum()) / buys['total'].sum()))
+    #print("Total in % " + str((sells['total'].sum() - buys['total'].sum()) / buys['total'].sum()))
+    #profit = sells.total.values - buys.total.values
+    #print("overall profit " + str(profit.sum()))
+    #rel_profit = (sells.total.values - buys.total.values) / buys.total.values
+    #print("overall profit in % " + str(rel_profit.mean()))
