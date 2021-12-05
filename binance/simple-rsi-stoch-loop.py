e4a1c1a6cd1043afbdbdff48d1374ef487b58211
schmidtjohannes <johannes.schmidt.delaunay@gmail.com>
2021-12-05 20:41:50 -0300

    switch to bnb

--- binance/simple-rsi-stoch-loop.py
+++ binance/simple-rsi-stoch-loop.py
@@ -15,87 +15,64 @@ SLEEP_INTERVAL_IN_SEC = 5
 SHORT_TERM = 5
 LONG_TERM = 8
 
-ETHUSDT_COIN = {
-  "bot_pair": "ETHUSDT",
-  "quantity": 0.001,
-  "asset": "ETH",
-  "float_lot": True
-}
-
-DOTUSDT_COIN = {
-  "bot_pair": "DOTUSDT",
+DOTBNB_COIN = {
+  "bot_pair": "DOTBNB",
   "quantity": 1,
   "asset": "DOT",
   "float_lot": True
 }
+
 # Non altcoin
-MANAUSDT_COIN = {
-  "bot_pair": "MANAUSDT",
-  "quantity": 10,
-  "asset": "MANA",
-  "float_lot": False
-}
-# Non altcoin
-LRCUSDT_COIN = {
-  "bot_pair": "LRCUSDT",
-  "quantity": 15,
-  "asset": "LRC",
-  "float_lot": False
-}
-# Non altcoin
-ENJUSDT_COIN = {
-  "bot_pair": "ENJUSDT",
+ENJBNB_COIN = {
+  "bot_pair": "ENJBNB",
   "quantity": 4,
   "asset": "ENJ",
   "float_lot": False
 }
 # Non altcoin
-FLOWUSDT_COIN = {
-  "bot_pair": "FLOWUSDT",
+FLOWBNB_COIN = {
+  "bot_pair": "FLOWBNB",
   "quantity": 0.2,
   "asset": "FLOW",
   "float_lot": True
 }
 
-IOTAUSDT_COIN = {
-  "bot_pair": "IOTAUSDT",
+IOTABNB_COIN = {
+  "bot_pair": "IOTABNB",
   "quantity": 2,
   "asset": "IOTA",
   "float_lot": False
 }
 
-BLZUSDT_COIN = {
-  "bot_pair": "BLZUSDT",
-  "quantity": 30,
-  "asset": "BLZ",
-  "float_lot": False
-}
-
-SANDUSDT_COIN = {
-  "bot_pair": "SANDUSDT",
+SANDBNB_COIN = {
+  "bot_pair": "SANDBNB",
   "quantity": 2,
   "asset": "SAND",
   "float_lot": False
 }
 
-MATICUSDT_COIN = {
-  "bot_pair": "MATICUSDT",
-  "quantity": 10,
+MATICBNB_COIN = {
+  "bot_pair": "MATICBNB",
+  "quantity": 20,
   "asset": "MATIC",
   "float_lot": False
 }
 
+GALABNB_COIN = {
+  "bot_pair": "GALABNB",
+  "quantity": 20,
+  "asset": "GALA",
+  "float_lot": False
+}
+
 AVAILABLE_COINS = { 
-    "MANA" : MANAUSDT_COIN ,
-    "DOT" : DOTUSDT_COIN,
-    "ETH" : ETHUSDT_COIN ,
-    "LRC" : LRCUSDT_COIN ,
-    "ENJ" : ENJUSDT_COIN ,
-    "FLOW" : FLOWUSDT_COIN ,
-    "IOTA" : IOTAUSDT_COIN ,
-    "BLZ" : BLZUSDT_COIN ,
-    "SAND" : SANDUSDT_COIN ,
-    "MATIC" : MATICUSDT_COIN
+    "DOT" : DOTBNB_COIN,
+    "ENJ" : ENJBNB_COIN ,
+    "FLOW" : FLOWBNB_COIN ,
+    "IOTA" : IOTABNB_COIN ,
+    "SAND" : SANDBNB_COIN  ,
+    "MATIC" : MATICBNB_COIN ,
+    "GALA" : GALABNB_COIN
 }
 
 COIN = {}
@@ -168,9 +145,10 @@ def main(coin, qty, stoploss, takeprofit):
         print('\nCheck BUY at ' + get_time())
         ohlc = get_ohlc_data(coin)
         add_stats(ohlc)
+
         sig = Signals(ohlc, 3)
         sig.decide()
-        time.sleep(1)
+
         current_close = ohlc.Close.iloc[-1]
 
         print('current_close ' + str(current_close))
