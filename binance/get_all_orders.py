f25cefba9a7df251325ff56b47b6be41fcd650d2
schmidtjohannes <johannes.schmidt.delaunay@gmail.com>
2021-12-05 20:41:22 -0300

    switch to bnb commission fee

--- binance/get_all_orders.py
+++ binance/get_all_orders.py
@@ -1,13 +1,14 @@
 from binance import Client
 import configparser
 import pandas as pd
-
+from datetime import datetime, timedelta
 import sys
 
 def calcTotal(row):
-    if row.isBuyer:
-        return (row['price'] * row['qty']) - (row['commission'] * row['price'])
-    else:
+    # switch to bnb no need to distinguish commission asset
+    # if row.isBuyer:
+    #     return (row['price'] * row['qty']) - (row['commission'] * row['price'])
+    # else:
         return (row['price'] * row['qty']) - row['commission']
 
 
@@ -18,40 +19,44 @@ if __name__ == '__main__':
     api_secret = config.get('BINANCE', 'SECRET')
     binance_client = Client(api_key, api_secret)
 
+    pd.set_option('display.precision',8)
+
     orders = binance_client.get_my_trades(symbol=sys.argv[1] + 'USDT', limit=100)
     df = pd.DataFrame(orders)
     df = df.set_index('time')
     df.index = pd.to_datetime(df.index, unit='ms')
 
-    df.drop(columns=['id','orderListId','quoteQty','commissionAsset','isBestMatch'], inplace=True)
+    #df.drop(columns=['id','orderListId','quoteQty','commissionAsset','isBestMatch'], inplace=True)
+    df.drop(columns=['id','orderListId','quoteQty','isBestMatch'], inplace=True)
 
+    today = datetime.today()
+    yesterday = yesterday = datetime.now() - timedelta(1)
+    lookup_date_yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
+    lookup_date = datetime.strftime(today, '%Y-%m-%d')
+    # BNB only
+    df = df[df['commissionAsset'] == 'BNB']
     # time range
-    df = df.loc['2021-12-03 00:00':'2021-12-04']
-    print(df)
+    df = df.loc[lookup_date_yesterday:lookup_date]
+
     # remove first row if SELL
-    if not df.iloc[0].isBuyer:
-        df = df.tail(len(df) - 1)
-    #df = df.tail(len(df) - 1)
+    while not df.iloc[0].isBuyer:
+       df = df.tail(len(df) - 1)
+
     # remove last row if BUY
-    if df.iloc[-1].isBuyer:
-        df = df.head(len(df) - 1)
-    print(df)
+    while df.iloc[-1].isBuyer:
+       df = df.head(len(df) - 1)
+
     df["price"] = pd.to_numeric(df["price"], downcast="float")
     df["qty"] = pd.to_numeric(df["qty"], downcast="float")
     df["commission"] = pd.to_numeric(df["commission"], downcast="float")
     #df['total'] = (df['price'] * df['qty']) - df['commission']
     df['total'] = df.apply(calcTotal, axis=1)
-
+    print(df)
     buys = df[df["isBuyer"] == True]
     sells = df[df["isBuyer"] == False]
 
-    print("Buys " + str(buys['total'].sum()))
-    print("Sells " + str(sells['total'].sum()))
+    print(str(len(buys)) + " Buys in $ " + str(buys['total'].sum()))
+    print(str(len(sells)) + " Sells in $ " + str(sells['total'].sum()))
 
     print("Total in $ " + str(sells['total'].sum() - buys['total'].sum()))
     print("Total in % " + str((sells['total'].sum() - buys['total'].sum()) / buys['total'].sum()))
-    #print("Total in % " + str((sells['total'].sum() - buys['total'].sum()) / buys['total'].sum()))
-    #profit = sells.total.values - buys.total.values
-    #print("overall profit " + str(profit.sum()))
-    #rel_profit = (sells.total.values - buys.total.values) / buys.total.values
-    #print("overall profit in % " + str(rel_profit.mean()))
