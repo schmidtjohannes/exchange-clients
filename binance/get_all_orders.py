e48b568059995c36a10de533d7c6e9ee3956120c
schmidtjohannes <johannes.schmidt.delaunay@gmail.com>
2021-12-15 11:05:10 -0300

    improve simple order revue

--- binance/get_all_orders.py
+++ binance/get_all_orders.py
@@ -11,54 +11,112 @@ def calcTotal(row):
     # if row.isBuyer:
     #     return (row['price'] * row['qty']) - (row['commission'] * row['price'])
     # else:
-        return (row['price'] * row['qty']) - row['commission']
+        #return (row['price'] * row['qty']) - row['commission']
+        return row['price'] * row['qty']
 
+def calcStats(symbol, current_bnb):
+   orders = binance_client.get_my_trades(symbol=symbol + 'BNB', limit=100)
+   if orders == []:
+      print("")
+      print("no trades found for " + symbol)
+      print("")
+      exit()
+   df = pd.DataFrame(orders)
+   df = df.set_index('time')
+   df.index = pd.to_datetime(df.index, unit='ms')
+
+   #df.drop(columns=['id','orderListId','quoteQty','commissionAsset','isBestMatch'], inplace=True)
+   df.drop(columns=['id','orderListId','quoteQty','isBestMatch'], inplace=True)
+
+   today = datetime.today()
+   yesterday = yesterday = datetime.now() - timedelta(1)
+   lookup_date_yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
+   lookup_date = datetime.strftime(today, '%Y-%m-%d')
+   # BNB only
+   df = df[df['commissionAsset'] == 'BNB']
+   # time range
+   df = df.loc[lookup_date_yesterday:lookup_date]
+   #df = df.loc['2021-12-08 10:30':'2021-12-08']
+
+   # remove first row if SELL
+   while not df.iloc[0].isBuyer:
+      df = df.tail(len(df) - 1)
+
+   # remove last row if BUY
+   while df.iloc[-1].isBuyer:
+      df = df.head(len(df) - 1)
+
+   df["price"] = pd.to_numeric(df["price"], downcast="float")
+   df["qty"] = pd.to_numeric(df["qty"], downcast="float")
+   df["commission"] = pd.to_numeric(df["commission"], downcast="float")
+   df['total'] = df.apply(calcTotal, axis=1)
+
+   buys = df[df["isBuyer"] == True]
+   sells = df[df["isBuyer"] == False]
+
+   print("\nResults for " + symbol)
+   print(str(len(buys)) + " Buys in USDT $ " + str(buys['total'].sum() * current_bnb))
+   print(str(len(sells)) + " Sells in USDT $ " + str(sells['total'].sum() * current_bnb))
+
+   print("Total in USDT $ " + str((sells['total'].sum() - buys['total'].sum()) * current_bnb))
+   print("Total Commission in USDT $ " + str(df['commission'].sum() * current_bnb))
+   print("Total Win minus commission in USDT $ " + str((sells['total'].sum() - buys['total'].sum() - df['commission'].sum()) * current_bnb))
+   print("Total in % " + str((sells['total'].sum() - buys['total'].sum() - df['commission'].sum()) / buys['total'].sum()))
+   
+   # print(df.loc[sells.index].price.values)
+   # print(df.loc[buys.index].price.values)
+   
+   # calc real profit per buy/sell pair
+   # total = 0.0
+   # commission = 0.0
+   # profits = []
+   # for i, row in df.iterrows():
+   #    print("")
+   #    print(str(i))
+   #    print(row['isBuyer'])
+   #    if not row['isBuyer']:
+   #       print('continue')
+   #       profits.append(total - commission)
+   #       total = 0.0
+   #       commission = 0.0
+   #       continue
+   #    total += (row['price'] * row['qty'])
+   #    commission += row['commission']
+   #    print(str(total))
+   #    for y, yrow in (sells[sells.index > i]).iterrows():
+   #       print('add sells')
+   #       print(yrow['isBuyer'])
+   #       if yrow['isBuyer']:
+   #          total -= (yrow['price'] * yrow['qty'])
+   #          commission += yrow['commission']
+   #          print(str(total))
+   #          break
+   #    print('append')
+   # print(profits)
+
+
+   # rel_profits = \
+   #    (df.loc[actualtrades.Selling_dates].Open.values - df.loc[actualtrades.Buying_dates].Open.values) / df.loc[actualtrades.Buying_dates].Open.values
+
+   # print("overall profit in % " + str(rel_profits.mean()))
+
+   # rel_profits = (rel_profits + 1).cumprod()
+
+   # print("overall profit in % " + str(rel_profits.mean()))
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read_file(open('.binance'))
-    api_key = config.get('BINANCE', 'KEY')
-    api_secret = config.get('BINANCE', 'SECRET')
-    binance_client = Client(api_key, api_secret)
+   config = configparser.ConfigParser()
+   config.read_file(open('.binance'))
+   api_key = config.get('BINANCE', 'KEY')
+   api_secret = config.get('BINANCE', 'SECRET')
+   binance_client = Client(api_key, api_secret)
 
-    pd.set_option('display.precision',8)
+   pd.set_option('display.precision',8)
+   # symbol = sys.argv[1]
 
-    orders = binance_client.get_my_trades(symbol=sys.argv[1] + 'BNB', limit=100)
-    df = pd.DataFrame(orders)
-    df = df.set_index('time')
-    df.index = pd.to_datetime(df.index, unit='ms')
+   ticker = binance_client.get_symbol_ticker(symbol="BNBUSDT")
+   current_bnb = float(ticker['price'])
 
-    #df.drop(columns=['id','orderListId','quoteQty','commissionAsset','isBestMatch'], inplace=True)
-    df.drop(columns=['id','orderListId','quoteQty','isBestMatch'], inplace=True)
-
-    today = datetime.today()
-    yesterday = yesterday = datetime.now() - timedelta(1)
-    lookup_date_yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
-    lookup_date = datetime.strftime(today, '%Y-%m-%d')
-    # BNB only
-    df = df[df['commissionAsset'] == 'BNB']
-    # time range
-    df = df.loc[lookup_date_yesterday:lookup_date]
-
-    # remove first row if SELL
-    while not df.iloc[0].isBuyer:
-       df = df.tail(len(df) - 1)
-
-    # remove last row if BUY
-    while df.iloc[-1].isBuyer:
-       df = df.head(len(df) - 1)
-
-    df["price"] = pd.to_numeric(df["price"], downcast="float")
-    df["qty"] = pd.to_numeric(df["qty"], downcast="float")
-    df["commission"] = pd.to_numeric(df["commission"], downcast="float")
-    #df['total'] = (df['price'] * df['qty']) - df['commission']
-    df['total'] = df.apply(calcTotal, axis=1)
-    print(df)
-    buys = df[df["isBuyer"] == True]
-    sells = df[df["isBuyer"] == False]
-
-    print(str(len(buys)) + " Buys in $ " + str(buys['total'].sum()))
-    print(str(len(sells)) + " Sells in $ " + str(sells['total'].sum()))
-
-    print("Total in $ " + str(sells['total'].sum() - buys['total'].sum()))
-    print("Total in % " + str((sells['total'].sum() - buys['total'].sum()) / buys['total'].sum()))
+   # for symbol in ['ENJ', 'DOT', 'MATIC', 'GALA', 'IOTA', 'SAND']:
+   for symbol in ['GALA', 'SAND', 'MATIC']:
+      calcStats(symbol, current_bnb)
